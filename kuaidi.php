d96586717a1fe695d08c76aa8b62f4a9bbccc871
曹润 <caorun@qq.com>
2015-01-08 13:24:36 +0800

    2015010801

---
+++ kuaidi.php
@@ -0,0 +1,7 @@
+﻿<?php  	
+                $com="yunda";  	
+                $nu="1900171113992"; 	
+                $context="查询";  
+                $link="<a href='http://www.kuaidi100.com/chaxun?com=".$com."&nu=".$nu."'>".$context."</a >";
+                echo $link; 
+                ?> 
