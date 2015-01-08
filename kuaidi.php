6fc3190aef64950a578d5288bd7b1d9761ee61fb
曹润 <caorun@qq.com>
2015-01-08 13:31:42 +0800

    2015010802Natasha Romanoff

--- kuaidi.php
+++
@@ -1,7 +0,0 @@
-﻿<?php  	
-                $com="yunda";  	
-                $nu="1900171113992"; 	
-                $context="查询";  
-                $link="<a href='http://www.kuaidi100.com/chaxun?com=".$com."&nu=".$nu."'>".$context."</a >";
-                echo $link; 
-                ?> 
