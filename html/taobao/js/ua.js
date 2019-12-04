fb66cf1461f53d779692165b423d37307b813aff
caorun <caorun@qq.com>
2019-12-04 13:09:06 +0800

    delete

--- html/taobao/js/ua.js
+++
@@ -1,8 +0,0 @@
-var Sys = {};
-var ua = navigator.userAgent.toLowerCase();
-//var i=ua.indexOf('micromessenger') ;
-var i=ua.indexOf('iphone') ;
-if(i<0)
-{
-  window.location.href="http://cr.ha.cn";
-}
